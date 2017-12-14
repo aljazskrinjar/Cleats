2ad417b3f7667016adbe2b6aad203ff1aa90d092
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-12-14 18:11:18 +0100

    added title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,1 +1,11 @@
+import React, { PureComponent } from 'React'
 
+class Title from extends PureComponent {
+  render() {
+      return(
+        <h1>{ this.props.content }</h1>
+      )
+  }
+}
+
+export default Title
