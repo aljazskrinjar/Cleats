91883adc7d996b7fe8aeb65dc34a73d8ea08c907
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-12-16 22:51:15 +0100

    added test for title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,10 @@
-import React, { PureComponent } from 'React'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
-class Title from extends PureComponent {
+class Title extends PureComponent {
+  static propTypes = {
+    content: PropTypes.string.isRequired,
+  }
   render() {
       return(
         <h1>{ this.props.content }</h1>
