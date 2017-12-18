55f51378630c029fa4121c06c3006535801d00fc
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-12-18 23:29:04 +0100

    even more tests

--- src/cleats/CleatsContainer.test.js
+++ src/cleats/CleatsContainer.test.js
@@ -4,12 +4,27 @@ import {shallow} from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
 import CleatsContainer from './CleatsContainer'
 import Title from '../components/Title'
-
+import CleatItem from './CleatItem'
 
 chai.use(chaiEnzyme())
 
+const cleats = [
+  {
+    title: 'Adidas Predator',
+    summary: 'Leather boot',
+  },
+  {
+    title: 'Nike Premiere Pro',
+    summary: 'Old school boot with modern touch'
+  },
+  {
+    title: 'Puma evoTouch',
+    summary: 'sock boot combined with kenguru leather'
+  },
+]
+
 describe('<CleatsContainer />', () => {
-  const container = shallow(<CleatsContainer cleats= {[]} />)
+  const container = shallow(<CleatsContainer cleats= {cleats} />)
 
   it('is wrapped in a div with class name "cleats"', () => {
     expect(container).to.have.className('wrapper')
@@ -17,5 +32,9 @@ describe('<CleatsContainer />', () => {
   })
   it('contains a Title', () => {
     expect(container).to.descendants(Title)
+  })
+  it('renders all cleats as a CleatItem', () => {
+    expect(container).to.have.exactly(3).descendants(CleatItem)
+
   })
 })
