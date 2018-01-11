3db1bef8f4a2dc57fd9f7ca6bda4903b6f0ab1d6
aljazskrinjar <aljaz.skrinjar@gmail.com>
2018-01-11 23:45:36 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
 import CleatsContainer from './cleats/CleatsContainer'
 
-const cleats = [
-  {
-    _id: 'a',
-    title: 'Adidas Predator',
-    summary: 'Leather boot',
-    liked: true,
-  },
-  {
-    _id: 'b',
-    title: 'Nike Premiere Pro',
-    summary: 'Old school boot with modern touch',
-    liked: false,
-  },
-  {
-    _id: 'c',
-    title: 'Puma evoTouch',
-    summary: 'sock boot combined with kenguru leather',
-    liked: false,
-  },
-]
-
 
 class App extends Component {
   render() {
       return (
         <div className="App">
-        <CleatsContainer cleats={cleats}/>
+        <CleatsContainer />
       </div>
     )
 }
